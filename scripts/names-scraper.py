31278343acf6d19faea9516a7cabc93717902677
vmmadathil <madathilvisakh@gmail.com>
2019-09-19 00:10:57 -0500

    scraper

--- scripts/names-scraper.py
+++ scripts/names-scraper.py
@@ -1,4 +1,5 @@
 import lyricsgenius
 genius = lyricsgenius.Genius("uWEvIQkyw6XS0jKsuOP5U-yhuUdPlR8sNWAq1ESdrrWhEqGg9HQQr1vt3ZF5X1cw")
-artist = genius.search_artist("Frank Ocean", max_songs=10, sort="title")
+artist = genius.search_artist("The Weeknd")
 print(artist.songs)
+lyrics = artist.save_lyrics()
