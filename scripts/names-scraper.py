e6b6bc8986a4164917cb020b17a36287f98a6a00
vmmadathil <madathilvisakh@gmail.com>
2019-09-19 00:11:23 -0500

    run on cloud?

--- scripts/names-scraper.py
+++ scripts/names-scraper.py
@@ -2,4 +2,16 @@ import lyricsgenius
 genius = lyricsgenius.Genius("uWEvIQkyw6XS0jKsuOP5U-yhuUdPlR8sNWAq1ESdrrWhEqGg9HQQr1vt3ZF5X1cw")
 artist = genius.search_artist("The Weeknd")
 print(artist.songs)
-lyrics = artist.save_lyrics()
+lyrics = artist.save_lyrics() 
+
+lyric_path = "lyrics.csv"
+lyrics.keys()
+songs = lyrics.get('songs')
+lyric_df = pd.DataFrame(columns=['name', 'lyrics'])
+for x in songs:
+    lyric_df = lyric_df.append({
+        'name': x.get('title'),
+        'lyrics': x.get('lyrics')
+    }, ignore_index=True)
+lyric_df.to_csv(lyric_path, index=False)
+lyric_df.iloc[0]
