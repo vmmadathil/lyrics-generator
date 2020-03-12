d19815c8c6a921d882caa3ea56d269256ab2bd9a
vmmadathil <madathilvisakh@gmail.com>
2020-03-11 23:40:12 -0500

    need to fix shapes

--- scripts/create-lyrics.py
+++ scripts/create-lyrics.py
@@ -19,8 +19,8 @@ int_to_char = dict((i, c) for i, c in enumerate(chars))
 # summarize the loaded data
 n_chars = len(raw_text)
 n_vocab = len(chars)
-print("Total Characters: " + n_chars)
-print("Total Vocab: "+ n_vocab)
+print("Total Characters: " + str(n_chars))
+print("Total Vocab: "+ str(n_vocab))
 # prepare the dataset of input to output pairs encoded as integers
 seq_length = 100
 dataX = []
@@ -31,9 +31,9 @@ for i in range(0, n_chars - seq_length, 1):
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
-print("Total Patterns: " +  n_patterns)
+print("Total Patterns: " +  str(n_patterns))
 # reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+X = np.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable
@@ -42,24 +42,25 @@ y = np_utils.to_categorical(dataY)
 model = Sequential()
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
+#model.add(LSTM(256))
+#
+#model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
 filename = "weights-improvement-01-2.9386.hdf5"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
+start = np.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
 print("Seed: ")
 print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 # generate characters
 for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = np.reshape(pattern, (1, len(pattern), 1))
 	x = x / float(n_vocab)
 	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
+	index = np.argmax(prediction)
 	result = int_to_char[index]
 	seq_in = [int_to_char[value] for value in pattern]
 	sys.stdout.write(result)
