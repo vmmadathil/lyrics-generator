35b20c031dc45d1b4f7e97a9bbab16d096dd7767
vmmadathil <madathilvisakh@gmail.com>
2020-08-13 02:29:57 -0500

    need to fix parsing lyrics

--- scripts/names-scraper.py
+++ scripts/names-scraper.py
@@ -1,30 +1,42 @@
 #import statements
 import lyricsgenius
 from dotenv import load_dotenv
+import pandas as pd
 import os
 
 load_dotenv()
+
 #fecthing the API key
 API_KEY = os.getenv("API_KEY")
 
+#creating the genius client
 genius = lyricsgenius.Genius(API_KEY)
 
+#fetching all the songs for the weeknd
 artist = genius.search_artist("The Weeknd")
 
-print(artist.songs)
+print('saving all the lyrics files')
 
-'''
-lyrics = artist.save_lyrics() 
+#creating a json of the lyrics data 
+artist.save_lyrics() 
 
+#getting the name and lyric and saving it to a csv
 lyric_path = "lyrics.csv"
+
+#reading in all the lyrics
+
+
 lyrics.keys()
+
+
+
 songs = lyrics.get('songs')
 lyric_df = pd.DataFrame(columns=['name', 'lyrics'])
-for x in songs:
+for song in songs:
+    print('Working on {}'.format(song))
     lyric_df = lyric_df.append({
-        'name': x.get('title'),
-        'lyrics': x.get('lyrics')
+        'name': song.get('title'),
+        'lyrics': song.get('lyrics')
     }, ignore_index=True)
 lyric_df.to_csv(lyric_path, index=False)
 lyric_df.iloc[0]
-'''
