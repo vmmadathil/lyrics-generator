c756f960164f40b72a3e492aadd6a060f6b902c9
vmmadathil <madathilvisakh@gmail.com>
2020-08-24 16:45:15 -0500

    some edits to the cleaning lyrics script

---
+++ scripts/clean-lyrics.py
@@ -0,0 +1,76 @@
+#this script will clean up the lyrics
+import re
+
+'''
+TO DO:
+    1. Remove all verses of features lyrics
+    2. Remove all verse tags
+    3. Change all words that end with "in'" to "ing" 
+    4. Remove all ad-libs (for now)
+'''
+
+
+'''
+Function: Remove features
+'''
+def removeFeatures(text):
+    #creating an array of lyrics chunks
+    verses = text.split('[')
+
+    #empty array
+    confirmedVerses = []
+
+    #for loop to loop through all the verses 
+    for verse in verses:
+        #seeing if the verse has a colon -- if it does, see if the verse is by The Weeknd
+        if ((':' in verse) and ('The Weeknd' in verse)):
+            confirmedVerses.append('[' + verse)
+        else:
+            confirmedVerses.append('[' + verse)
+    
+    #calling function to remove verse tags
+    removeTags(confirmedVerses)
+
+
+'''
+Function: Remove tags
+'''
+def removeTags(verses):
+    
+    print(type(verses[1]))
+    print((verses[1]))
+    #looping through all the verses
+    #for verse in verses:
+        #verse, *_ = verse.split(']')
+        
+        #print(verse)
+
+
+
+'''
+Function: Replace words
+'''
+
+'''
+Function: Remove Ad-libs
+'''
+
+
+#main function
+def main():
+    #importing in all the lyrics
+    with open('../data/pre-processed/raw_lyrics.txt', 'r',  encoding="utf-8") as f:
+        text = f.read()
+    
+    print('read in lyrics, removing featured artists')
+    removeFeatures(text)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#importing in all the lyrics
--- scripts/lyrics-scraper.py
+++ scripts/lyrics-scraper.py
@@ -19,24 +19,3 @@ print('saving all the lyrics files')
 
 #creating a json of the lyrics data 
 artist.save_lyrics() 
-
-#getting the name and lyric and saving it to a csv
-lyric_path = "lyrics.csv"
-
-#reading in all the lyrics
-
-
-lyrics.keys()
-
-
-
-songs = lyrics.get('songs')
-lyric_df = pd.DataFrame(columns=['name', 'lyrics'])
-for song in songs:
-    print('Working on {}'.format(song))
-    lyric_df = lyric_df.append({
-        'name': song.get('title'),
-        'lyrics': song.get('lyrics')
-    }, ignore_index=True)
-lyric_df.to_csv(lyric_path, index=False)
-lyric_df.iloc[0]
