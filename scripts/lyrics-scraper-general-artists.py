cb04c1e7e371262b5728733e8a359be97695d48f
vmmadathil <madathilvisakh@gmail.com>
2020-09-22 00:26:14 -0500

    more work on the script to get more lyrics

--- scripts/lyrics-scraper-general-artists.py
+++ scripts/lyrics-scraper-general-artists.py
@@ -6,16 +6,30 @@ import os
 
 load_dotenv()
 
+#After training the first iteration of the model, I recognized that I need more data for the model to make sense. Here, I'll begin augmenting the data set with some other R&B artists
+
 #fetching the API key
 API_KEY = os.getenv("API_KEY")
 
-#creating the genius client
-genius = lyricsgenius.Genius(API_KEY)
+#progress messages
+print('attempting to connect to Genius API')
 
-#fetching all the songs for the weeknd
-artist = genius.search_artist("The Weeknd")
+try:
+    #creating the genius client
+    genius = lyricsgenius.Genius(API_KEY)
+    print('successfully connected')
+except:
+    print('failed to connect!')
 
-print('saving all the lyrics files')
+#a list of artists I'm interested in
+artists = ['Frank Ocean', 'Bryson Tiller', 'Miguel', 'Usher', 'Tory Lanez', 'Trey Songz', 'dvsn', 'Rihanna', 'James Blake']
 
-#creating a json of the lyrics data 
-artist.save_lyrics() 
+#iterate through all these artists, keep only their top 15 songs
+for name in artists:
+    try:
+        artist = genius.search_artist(name, max_songs = 15)
+        print('Saving the songs for {}').format(name)
+        artist.save_lyrics()
+    except:
+        print("failed to save lyrics for {}").format(name) 
+
