e7cfacddcfd596bcd00934f3d3c2930430b4a697
vmmadathil <madathilvisakh@gmail.com>
2020-09-22 00:22:54 -0500

    code to scrape more artists

---
+++ scripts/lyrics-scraper-general-artists.py
@@ -0,0 +1,21 @@
+#import statements
+import lyricsgenius
+from dotenv import load_dotenv
+import pandas as pd
+import os
+
+load_dotenv()
+
+#fetching the API key
+API_KEY = os.getenv("API_KEY")
+
+#creating the genius client
+genius = lyricsgenius.Genius(API_KEY)
+
+#fetching all the songs for the weeknd
+artist = genius.search_artist("The Weeknd")
+
+print('saving all the lyrics files')
+
+#creating a json of the lyrics data 
+artist.save_lyrics() 
